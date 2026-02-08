acdc797e4c8ce3faa85279fbfe387e4a7ba717a0
Victor Chardes <vchardes@worker6132.cm.cluster>
2026-02-07 21:13:32 -0500

    deploy

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+mkdocs build
+if git status | grep -q "Your branch is up to date"; then
+    echo "Branch is up to date."
+else
+    echo "Branch is not up to date."
+    git commit -am "new docs"
+    git push
+fi
+
+git checkout gh-pages
+git rm -r --ignore-unmatch ./*
+git checkout main -- ./site
+git mv ./site/* ./
+git add . 
+
+if git status | grep -q "Your branch is up to date"; then
+    echo "Branch is up to date."
+else
+    echo "Branch is not up to date."
+    git commit -am "updating the project page"
+    git push
+fi
+
+git checkout main
